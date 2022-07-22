3fe3bf3730a3dfa8eae0064bcf0b0c64b79a1c38
Sergey Petrinich <petrinichshadow@gmail.com>
2022-07-22 15:08:01 +0300

    how v2

--- src/_containers/HomePage/How.jsx
+++ src/_containers/HomePage/How.jsx
@@ -1,50 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useEvent } from "../../helpers/useEvent";
 
 const How = () => {
+    const [mobileGrid, setMobileGrid] = useState(null);
+
+    const isMobile = () => {
+        if (window.innerWidth < 767) {
+            setMobileGrid(true);
+        } else {
+            setMobileGrid(false);
+        }
+    };
+
+    useEffect(() => {
+        isMobile();
+    }, []);
+
+    useEvent("resize", () => isMobile());
+
     return (
         <section className="page__how how">
             <div className="how__container">
                 <h3 className="how__title">How it works</h3>
-                <div className="how__row-top">
-                    <div className="how__column">
-                        <div className="how__body">
-                            <h4 className="how__top _icon-graphic">Designer Consultation</h4>
-                            <div className="how__text">
-                                Kinfolk scenester authentic craft beer truffaut irony intelligentsia YOLO lomo bushwick coloring
-                                book. Semiotics man bun venmo viral cliche
+                {mobileGrid ? (
+                    <div className="how__row-top">
+                        <div className="how__column">
+                            <div className="how__body">
+                                <h4 className="how__top _icon-graphic">Designer Consultation</h4>
+                                <div className="how__text">
+                                    Kinfolk scenester authentic craft beer truffaut irony intelligentsia YOLO lomo bushwick
+                                    coloring book. Semiotics man bun venmo viral cliche
+                                </div>
+                            </div>
+                        </div>
+                        <div className="how__column">
+                            <div className="how__body">
+                                <h4 className="how__top _icon-measur">Measurements</h4>
+                                <div className="how__text">
+                                    Intelligentsia YOLO lomo bushwick coloring book. Semiotics man bun venmo viral cliche
+                                </div>
+                            </div>
+                        </div>
+                        <div className="how__column">
+                            <div className="how__body">
+                                <h4 className="how__top _icon-sofa">Production</h4>
+                                <div className="how__text">Everyday carry actually neutra authentic kogi shabby chic</div>
+                            </div>
+                        </div>
+                        <div className="how__column">
+                            <div className="how__body">
+                                <h4 className="how__top _icon-graphic">Designer Consultation</h4>
+                                <div className="how__text">
+                                    Kinfolk scenester authentic craft beer truffaut irony intelligentsia YOLO lomo bushwick
+                                    coloring book.
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="how__column" data-da=".how__row-bottom,767,2"></div>
-                    <div className="how__column">
-                        <div className="how__body">
-                            <h4 className="how__top _icon-sofa">Production</h4>
-                            <div className="how__text">Everyday carry actually neutra authentic kogi shabby chic</div>
+                ) : (
+                    <>
+                        <div className="how__row-top">
+                            <div className="how__column">
+                                <div className="how__body">
+                                    <h4 className="how__top _icon-graphic">Designer Consultation</h4>
+                                    <div className="how__text">
+                                        Kinfolk scenester authentic craft beer truffaut irony intelligentsia YOLO lomo bushwick
+                                        coloring book. Semiotics man bun venmo viral cliche
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="how__column"></div>
+                            <div className="how__column">
+                                <div className="how__body">
+                                    <h4 className="how__top _icon-sofa">Production</h4>
+                                    <div className="how__text">Everyday carry actually neutra authentic kogi shabby chic</div>
+                                </div>
+                            </div>
+                            <div className="how__column"></div>
                         </div>
-                    </div>
-                    <div className="how__column" data-da=".how__row-bottom,767,4"></div>
-                </div>
-                <div className="how__row-bottom">
-                    <div className="how__column"></div>
-                    <div className="how__column" data-da=".how__row-top,767,2">
-                        <div className="how__body">
-                            <h4 className="how__top _icon-measur">Measurements</h4>
-                            <div className="how__text">
-                                Intelligentsia YOLO lomo bushwick coloring book. Semiotics man bun venmo viral cliche
+                        <div className="how__row-bottom">
+                            <div className="how__column"></div>
+                            <div className="how__column">
+                                <div className="how__body">
+                                    <h4 className="how__top _icon-measur">Measurements</h4>
+                                    <div className="how__text">
+                                        Intelligentsia YOLO lomo bushwick coloring book. Semiotics man bun venmo viral cliche
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="how__column"></div>
+                            <div className="how__column">
+                                <div className="how__body">
+                                    <h4 className="how__top _icon-graphic">Designer Consultation</h4>
+                                    <div className="how__text">
+                                        Kinfolk scenester authentic craft beer truffaut irony intelligentsia YOLO lomo bushwick
+                                        coloring book.
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="how__column"></div>
-                    <div className="how__column" data-da=".how__row-top,767,4">
-                        <div className="how__body">
-                            <h4 className="how__top _icon-mail">Designer Consultation</h4>
-                            <div className="how__text">
-                                Kinfolk scenester authentic craft beer truffaut irony intelligentsia YOLO lomo bushwick coloring
-                                book.
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    </>
+                )}
             </div>
         </section>
     );
