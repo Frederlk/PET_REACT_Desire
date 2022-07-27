ab96eb66e57de467edc965119351dda70b848848
Sergey Petrinich <petrinichshadow@gmail.com>
2022-07-27 18:27:50 +0300

    blog-fill v2 \ minor-changes

--- src/_containers/BlogPage/BlogFill.jsx
+++ src/_containers/BlogPage/BlogFill.jsx
@@ -2,30 +2,17 @@ import React, { useEffect, useState } from "react";
 import { data } from "../../constants";
 import BlogArticle from "./BlogArticle";
 import ReactPaginate from "react-paginate";
+import { sortData } from "../../helpers/sortData";
+import { gotoBlock } from "../../js/files/scroll/gotoblock";
 
 const BlogFill = () => {
-    const [itemsPerPage, setItemsPerPage] = useState(1);
-    const [blogItems, setBlogItems] = useState([]);
+    const [itemsPerPage, setItemsPerPage] = useState(2);
+    const [blogItems, setBlogItems] = useState(data?.blogItems.slice().sort((a, b) => sortData(a.date, b.date)));
     const [currentItems, setCurrentItems] = useState([]);
     const [pageCount, setPageCount] = useState(0);
     const [itemOffset, setItemOffset] = useState(0);
 
-    const sortPosts = (a, b) => {
-        if (a > b) return -1;
-        if (a < b) return 1;
-        return 0;
-    };
-
-    useEffect(() => {
-        setBlogItems(
-            data?.blogItems
-                .slice()
-                .sort((a, b) => {
-                    return sortPosts(a.date, b.date);
-                })
-                .map((item, i) => <BlogArticle key={item.link + i} textBlock item={item} />)
-        );
-    }, []);
+    console.log(blogItems);
 
     useEffect(() => {
         const endOffset = itemOffset + itemsPerPage;
@@ -34,26 +21,29 @@ const BlogFill = () => {
     }, [itemOffset, itemsPerPage]);
 
     const handlePageClick = (e) => {
+        gotoBlock(".breadcrumbs");
         const newOffset = (e.selected * itemsPerPage) % blogItems.length;
         setItemOffset(newOffset);
     };
 
     return (
         <div className="blog-content__fill fill">
-            <div className="fill__items">{blogItems}</div>
+            <div className="fill__items">
+                {currentItems.length
+                    ? currentItems.map((item, i) => <BlogArticle key={item.link + i} textBlock item={item} />)
+                    : null}
+            </div>
             {pageCount < 1 ? null : (
                 <ReactPaginate
                     className="fill__pagination"
-                    pageLinkClassName="pagging__item"
-                    breakClassName="pagging__item"
                     activeLinkClassName="_active"
                     breakLabel="..."
                     onPageChange={handlePageClick}
                     pageRangeDisplayed={2}
                     marginPagesDisplayed={1}
                     pageCount={pageCount}
-                    previousLabel={<span className="_icon-arrow-next" />}
-                    nextLabel={<span className="_icon-arrow-next" />}
+                    previousLabel={<span className="prev _icon-arrow-next" />}
+                    nextLabel={<span className="next _icon-arrow-next" />}
                 />
             )}
         </div>
