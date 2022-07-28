c0892057b7258086d8283275ae8aa63ef80957cd
Sergey Petrinich <petrinichshadow@gmail.com>
2022-07-28 17:27:38 +0300

    blog-page v2.1

--- src/_pages/BlogPage.jsx
+++ src/_pages/BlogPage.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 import { useLocation } from "react-router-dom";
 import { Breadcrumbs } from "../_components";
 import { BlogFill, BlogSide } from "../_containers";
 
 const BlogPage = () => {
     let categoryLocation = useLocation();
-    const [category, setCategory] = useState(categoryLocation.state.category || null);
+    const [category, setCategory] = useState(categoryLocation?.state?.category || null);
     const [tag, setTag] = useState(null);
     const [search, setSearch] = useState("");
     return (
         <>
+            <Helmet>
+                <meta name="Desire - Blog" content="Desire - Blog" />
+                <title>Desire - Blog</title>
+            </Helmet>
+
             <Breadcrumbs className="breadcrumbs_blog" noBg />
 
             <div className="page__blog-content blog-content">
